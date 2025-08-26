97136e2833e84bf801451ef4833a93301a68b873
Krishnnagarg <garg79408@gmail.com>
2025-08-26 09:57:53 +0530

    shortId Shortener

--- controllers/url.js
+++ controllers/url.js
@@ -1,18 +1,18 @@
 import shortid from "shortid";
 import { URL } from "../models/url.js";
 
-async function handleGenerateNewShortURL(req,res) {
-    const body = req.body;
-    if(!body.url) return res.status(400).json({ error: "Url Is Required"});
-    const shortID = shortid.generate();
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "Url Is Required" });
+  const shortID = shortid();
 
-    await URL.create({
-        shortID: shortID,
-        redirectURL:body.url,
-        visitHistory: [],
-    });
-
-    return res.json({ id : shortID});
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  
+  return res.json({ id: shortID });
 }
 
-export {handleGenerateNewShortURL};
+export { handleGenerateNewShortURL };
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import path from "path";
 import urlRouter from "./routes/url.js";
 import { connectMongoDB } from "./connection.js";
 import { URL } from "./models/url.js";
+import staticRoute from "./routes/staticRouter.js";
 
 const app = express();
 const PORT = 8001;
@@ -10,11 +12,16 @@ connectMongoDB("mongodb://127.0.0.1:27017/short_url")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Error in MongoDB is:", err));
 
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRouter);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
@@ -31,7 +38,7 @@ app.get("/:shortId", async (req, res) => {
   if (!entry) {
     return res.status(404).json({ error: "Short URL not found" });
   }
-  
+
   res.redirect(entry.redirectURL);
 });
 
